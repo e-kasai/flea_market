5fcdb1bb12e4716263635efb3e619802fac6062e
ekasai <ekasai.dev@gmail.com>
2025-09-03 10:47:54 +0900

    商品検索機能のコードを一部修正

--- src/app/Http/Controllers/ItemController.php
+++ src/app/Http/Controllers/ItemController.php
@@ -15,24 +15,21 @@ class ItemController extends Controller
         //tab指定がない場合のデフォルトはおすすめタブ
         $activeTab = $request->get('tab', 'recommend');
 
-        // 未ログイン状態の場合マイリストは何も表示しない
-        if ($activeTab === 'mylist' && !auth()->check()) {
-            $myListItems = collect();
-        }
-
         // おすすめ（検索条件があれば絞り込み）
         $items = Item::when($keyword, function ($query, $keyword) {
             return $query->where('item_name', 'LIKE', "%{$keyword}%");
         })->paginate(12)->withQueryString();
 
-        // マイリスト（検索条件があれば絞り込み）
-        // まだいいね機能実装前なのでお気に入り商品は空配列にしておく
-        $myListItems = collect();
-        if ($activeTab === 'mylist') {
+
+        // マイリストはログイン状態の場合のみ表示
+        if ($activeTab === 'mylist' && auth()->check()) {
             $myListItems = auth()->user()->favoriteItems()
                 ->when($keyword, fn($query, $keyword) =>
                 $query->where('item_name', 'LIKE', "%{$keyword}%"))->paginate(12)->withQueryString();
+        } else {
+            $myListItems = collect();
         }
+
         return view('index', [
             'activeTab' => $activeTab,
             'items'     => $items,
