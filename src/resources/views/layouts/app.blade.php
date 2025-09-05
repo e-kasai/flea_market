f216c1f913f64660043b14854b9ef710d4e5db14
ekasai <ekasai.dev@gmail.com>
2025-09-06 06:42:24 +0900

    ログインリダイレクト時UX改善の為フラッシュメッセージを追加

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -5,12 +5,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="{{ asset("css/sanitize.css") }}" />
         <link rel="stylesheet" href="{{ asset("css/common.css") }}" />
+        {{-- Font Awesome v6（solid/regular/brands まとめて） --}}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
         {{-- ページ専用CSS --}}
         @stack("styles")
     </head>
     <body>
         @include("layouts.header")
         <main>
+            @if (session("message"))
+                <div class="session-message">
+                    {{ session("message") }}
+                </div>
+            @endif
+
             @yield("content")
         </main>
     </body>
