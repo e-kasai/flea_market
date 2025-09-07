7e437da110a4d12437be734f018e87d173ccf683
ekasai <ekasai.dev@gmail.com>
2025-09-07 14:30:21 +0900

    TransactionRequestのfillable修正

---
+++ src/app/Http/Requests/TransactionRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TransactionRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        // return true;
+        // ログインかつプロフィール必須
+        return auth()->check() && auth()->user()->profile()->exists();
+    }
+
+    public function rules(): array
+    {
+        // 住所などはProfileRequestでvalidation済みなのでruleを書かない
+        return [
+            'payment_method' => ['required', 'in:1,2'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'payment_method.required' => '支払い方法を選択してください',
+            'payment_method.in' => '支払い方法を選択してください',
+        ];
+    }
+}
