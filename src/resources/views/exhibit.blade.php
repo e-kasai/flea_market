d754a708d6da67012272e54d549c0af25bafed9c
ekasai <ekasai.dev@gmail.com>
2025-09-09 19:41:23 +0900

    出品画面のCSSとレスポンシブ対応作成

--- src/resources/views/exhibit.blade.php
+++ src/resources/views/exhibit.blade.php
@@ -5,73 +5,74 @@
     <link rel="stylesheet" href="{{ asset("css/exhibit.css") }}" />
 @endpush
 
-{{-- 後でエラー一覧は消す予定 --}}
 @section("content")
-    @if ($errors->any())
-        <ul>
-            @foreach ($errors->all() as $e)
-                <li>{{ $e }}</li>
-            @endforeach
-        </ul>
-    @endif
-
     {{-- 共通レイアウトに差し込む部分 --}}
     <x-form.card title="" action="{{ route('exhibit.store') }}" method="POST" enctype="multipart/form-data">
         {{-- ----------------------------------- --}}
-        <section class="item-create">
-            <h1 class="item-create__title">商品の出品</h1>
-            <div class="item-create__image">
-                <div class="image-path__group">
-                    @if ($item->image_path)
-                        <img class="item" src="{{ asset("storage/" . $item->image_path) }}" alt="商品画像" />
-                    @endif
-                </div>
-                {{-- 画像選択リンク --}}
-                <div class="image-path">
-                    <h2 class="image-path__title">商品画像</h2>
-                    <label class="image-path__label" for="image_path">画像を選択する</label>
-                    <input class="image-path__input" type="file" id="image_path" name="image_path" />
-                    @error("image_path")
-                        <div class="form-group__error">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
+        <h1 class="form-card__title">商品の出品</h1>
+
+        <section class="avatar">
+            @if ($item->image_path)
+                <img class="item" src="{{ asset("storage/" . $item->image_path) }}" alt="商品画像" />
+            @endif
+
+            {{-- 画像選択リンク --}}
+            <span>商品画像</span>
+            <div class="avatar-path">
+                <label class="avatar-path__label" for="image_path">画像を選択する</label>
+                <input class="avatar-path__input" type="file" id="image_path" name="image_path" />
             </div>
+            @error("image_path")
+                <div class="form-group__error">
+                    {{ $message }}
+                </div>
+            @enderror
+        </section>
 
-            <h2 class="item-create__subtitle">商品の詳細</h2>
+        <div class="subtitle">
+            <h2 class="form-card__subtitle">商品の詳細</h2>
+        </div>
 
-            {{-- カテゴリチェックボックス --}}
-            <div class="item-create__categories">
+        <div class="categories">
+            <span>カテゴリー</span>
+            <div class="categories__list">
                 @foreach ($categories as $category)
-                    <label>
+                    <label class="category-tag">
                         <input
                             type="checkbox"
                             name="category_ids[]"
                             value="{{ $category->id }}"
                             {{ in_array($category->id, old("category_ids", [])) ? "checked" : "" }}
                         />
-                        {{ $category->category_name }}
+                        <span>{{ $category->category_name }}</span>
                     </label>
                 @endforeach
+            </div>
 
-                @error("category_ids")
-                    <div class="form-group__error">{{ $message }}</div>
-                @enderror
-            </div>
-            {{-- 商品の状態 select --}}
-            <div class="item-create__condition">
-                <select name="condition" required>
-                    <option value="3">良好</option>
-                    <option value="2">目立った傷や汚れなし</option>
-                    <option value="1">やや傷や汚れあり</option>
-                    <option value="0">状態が悪い</option>
-                </select>
-                @error("condition")
-                    <div class="form-group__error">{{ $message }}</div>
-                @enderror
-            </div>
-        </section>
+            @error("category_ids")
+                <div class="form-group__error">{{ $message }}</div>
+            @enderror
+        </div>
+
+        {{-- 商品の状態 select --}}
+        <div class="condition">
+            <label for="condition"><span>商品の状態</span></label>
+
+            <select class="condition__select" id="condition" name="condition" required>
+                <option value="" disabled selected hidden>選択してください</option>
+                <option value="3">良好</option>
+                <option value="2">目立った傷や汚れなし</option>
+                <option value="1">やや傷や汚れあり</option>
+                <option value="0">状態が悪い</option>
+            </select>
+
+            @error("condition")
+                <div class="form-group__error">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="subtitle">
+            <h2 class="form-card__subtitle">商品名と説明</h2>
+        </div>
         {{-- -------------------------------------------- --}}
         {{-- 名前 --}}
         <x-form.input type="text" name="item_name" label="商品名" value="{{ old('item_name') }}" required />
@@ -82,11 +83,12 @@
             {{ old("description") }}
         </x-form.input>
         {{-- 販売価格 --}}
+        <div class="price-input--with-symbol">
         <x-form.input type="text" name="price" label="販売価格" step="1" required />
+        </div>
         {{-- ボタン --}}
         <x-slot name="actions">
             <button class="btn" type="submit">出品する</button>
         </x-slot>
-        {{-- ここまでが$slotに入る --}}
     </x-form.card>
 @endsection
