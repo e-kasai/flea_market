aeabb1cca6e040b4669fe834baa81c6f0d835fa8
ekasai <ekasai.dev@gmail.com>
2025-09-12 14:46:02 +0900

    不要カラムを削除

--- src/database/migrations/2025_09_01_170635_create_transactions_table.php
+++ src/database/migrations/2025_09_01_170635_create_transactions_table.php
@@ -23,16 +23,6 @@ class CreateTransactionsTable extends Migration
             // 支払い・金額関連
             $table->tinyInteger('payment_method');
             $table->unsignedInteger('purchase_price'); // JPYの整数
-            // 'payment_status' = pendingにしておく（stripe導入後もmigration修正が不要）
-            $table->tinyInteger('payment_status')->default(0); // 0=pending, 1=paid, 2=canceled
-
-            // 支払い・キャンセル日時
-            $table->timestamp('paid_at')->nullable();
-            $table->timestamp('canceled_at')->nullable();
-
-            // Stripe関連
-            $table->string('payment_intent_id', 64)->nullable();
-            $table->string('charge_id', 64)->nullable();
 
             // 配送先情報
             $table->char('shipping_postal_code', 8); // ハイフンあり8文字
